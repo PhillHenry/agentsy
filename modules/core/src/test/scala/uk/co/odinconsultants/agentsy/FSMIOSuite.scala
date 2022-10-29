8010bf7778cfc08f6a5dad4b02cda8811be1a307
PhillipHenry <PhillHenry@gmail.com>
2022-10-29 09:57:28 +0100

    Refactored.

--- modules/core/src/test/scala/uk/co/odinconsultants/agentsy/FSMIOSuite.scala
+++ modules/core/src/test/scala/uk/co/odinconsultants/agentsy/FSMIOSuite.scala
@@ -3,12 +3,12 @@ import weaver.SimpleIOSuite
 import weaver.scalacheck.Checkers
 import cats.effect.IO
 
-object FSMSuite  extends SimpleIOSuite with Checkers {
+object FSMIOSuite extends SimpleIOSuite with Checkers {
   test("state transition") {
     val model = new StateModel[IO]
-    val intialState = FSM(model.run)
+    val initialState = FSM(model.run)
     for {
-      (state, _)  <- intialState.run(100, 1)
+      (state, _)  <- initialState.run(100, 1)
     } yield {
       expect.same(state, 101)
     }
