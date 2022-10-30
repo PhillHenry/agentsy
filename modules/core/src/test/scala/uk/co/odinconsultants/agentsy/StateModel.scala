5a5c40abf42665c79fc06b60012dcd74d86933b5
PhillipHenry <PhillHenry@gmail.com>
2022-10-30 10:31:33 +0000

    Formatting and attempt to model health care flows.

--- modules/core/src/test/scala/uk/co/odinconsultants/agentsy/StateModel.scala
+++ modules/core/src/test/scala/uk/co/odinconsultants/agentsy/StateModel.scala
@@ -2,14 +2,33 @@ package uk.co.odinconsultants.agentsy
 
 import cats.Applicative
 
-class StateModel[T[_]: Applicative] {
-  type State = Long
-  type Input = Int
+trait Model[T[_]: Applicative] {
+  type State
+  type Input
+  type Output
+  type StateTransition = (State, Input) => T[(State, Output)]
+}
+
+case class HealthCareDemand(gp: Int, emergency: Int, ambulance: Int)
+
+class HealthCareModel[T[_]: Applicative] extends Model[T] {
+  type State  = HealthCareDemand
+  type Input  = Int
   type Output = Unit
+  val initialState: StateTransition = (state, input) =>
+    Applicative[T].pure {
+      ???
+    }
+}
+
+class StateModel[T[_]: Applicative] extends Model[T] {
+  type State           = Long
+  type Input           = Int
+  type Output          = Unit
   type StateTransition = (State, Input) => T[(State, Output)]
   val run: StateTransition = (state, input) =>
-    Applicative[T].pure( {
+    Applicative[T].pure {
       val newState: Long = state + input
       (newState, println(s"$state - $input -> $newState"))
-    })
+    }
 }
